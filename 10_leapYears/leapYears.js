04e839dd1740d0d991f703473550ed08f3110689
FPabloA <pabloayala@protonmail.com>
2025-07-17 13:15:21 -0500

    finished exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isDivisibleByFour = year % 4 == 0;
+    let isDivisibleByHundred = year % 100 == 0;
+    let isDivisibleByFourHundred = year % 400 == 0;
 
+    if ((isDivisibleByFour && !isDivisibleByHundred) || isDivisibleByFourHundred){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
