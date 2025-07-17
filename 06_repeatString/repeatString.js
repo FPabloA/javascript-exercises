e3bb3561e29d5a05b77d34f4945a75fe0b3483c0
FPabloA <pabloayala@protonmail.com>
2025-07-17 12:47:24 -0500

    finished js exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeat) {
+    if (numRepeat < 0) {
+        return 'ERROR';
+    }
+    
+    res = ''
+    for (let i = 0; i < numRepeat; i++){
+        res = res + str
+    }
+    return res;
 };
 
 // Do not edit below this line
